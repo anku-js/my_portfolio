886819ba3806b0ef2580e07ea7eb3502f1a5030e
Anku Kumari <ankuhere96@gmail.com>
2023-04-27 11:15:56 +0530

    set projects page layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,21 @@
 export default function Projects() {
-    return (
-        <div>
-proj
-        </div>
-    )
+  return (
+    <div className="projectlist-wrapper">
+      <p className="projectlist-p">These are some of my projects...</p>
+      <div className="projectlist">       
+          <a>
+            <img />
+            <p className="project-title">Friend's List</p>
+          </a>
+          <a>
+            <img />
+            <p>Tenxies</p>
+          </a>
+          <a>
+            <img />
+            <p>Shopping</p>
+          </a>
+      </div>
+    </div>
+  );
 }
