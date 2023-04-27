1a6f3223ffbc4c15bf16128b76c1f9d27c46c7d0
Anku Kumari <ankuhere96@gmail.com>
2023-04-27 14:45:40 +0530

    adding alt text to images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ export default function Projects() {
       <p className="projectlist-p">These are some of my projects...</p>
       <div className="projectlist">
         <a href="" target="_blank" className="project">
-          <img className="project-image" src="friends.jpg" />
+          <img className="project-image" src="friends.jpg" alt="screenshot of friend's list project"/>
           <p className="project-title">Friend's List</p>
         </a>
         <a
@@ -12,7 +12,7 @@ export default function Projects() {
           target="_blank"
           className="project"
         >
-          <img className="project-image" src="shopping.jpg" />
+          <img className="project-image" src="shopping.jpg" alt="screenshot of shopping app project"/>
           <p className="project-title">Shopping</p>
         </a>
         <a
@@ -20,7 +20,7 @@ export default function Projects() {
           target="_blank"
           className="project"
         >
-          <img className="project-image" src="tenzies.jpg" />
+          <img className="project-image" src="tenzies.jpg" alt="screenshot of tenzies game project"/>
           <p className="project-title">Tenzies</p>
         </a>
       </div>
