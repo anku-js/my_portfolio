67861f995c9f9416cafebfd11f395550944514ff
Anku Kumari <ankuhere96@gmail.com>
2023-05-01 13:24:21 +0530

    added hamburger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import { FaReact } from "react-icons/fa";
 
 export default function Header() {
+const [ hamburgerActive, setHamburgerActive] = useState(false)
+
+  function handleClick() {
+    setHamburgerActive(hamburgerActive => !hamburgerActive)
+  }
+
   return (
     <div className="header-wrapper">
       <nav className="header">
@@ -12,17 +19,22 @@ export default function Header() {
             <h1>Anku Kumari</h1>
           </div>
         </div>
-        <ul className="nav-list">
+        <ul className={`nav-list ${hamburgerActive? "active" : ""}`}>
           <li>
-            <a href="#introduction-container">About</a>
+            <a onClick={handleClick} href="#introduction-container">About</a>
           </li>
           <li>
-            <a href="#projectlist-container">Projects</a>
+            <a onClick={handleClick}  href="#projectlist-container">Projects</a>
           </li>
           <li>
-            <a href="#footer-container">Contact</a>
+            <a onClick={handleClick}  href="#footer-container">Contact</a>
           </li>
         </ul>
+        <div className={`hamburger ${hamburgerActive? "active" : ""}`} onClick={handleClick}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>        
       </nav>
     </div>
   );
